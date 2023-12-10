8864b70b798395659703f01255e0f66ce035f729
Michellah <hei.michellah@gmail.com>
2023-12-10 07:27:12 +0300

    feat: add changement to code

--- Wallet.java
+++ Wallet.java
@@ -2,97 +2,87 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Wallet {
-    static double balance = 0;
-    static List<String> transactionList = new ArrayList<>();
-    static List<Account> accounts = new ArrayList<>();
-    static boolean isAuthenticated = false;
+    private static int balance = 0;
+    private static List<String> images = new ArrayList<>();
+    private static List<String> creditCards = new ArrayList<>();
+    private static List<String> driverLicense = new ArrayList<>();
+    private static List<String> cin = new ArrayList<>();
+    private static List<String> visitingCard = new ArrayList<>();
 
-    static class Account {
-        String username;
-        String password;
-
-        public Account(String username, String password) {
-            this.username = username;
-            this.password = password;
-        }
-    }
-
-    public static void createAccount(String username, String password) {
-        Account existingAccount = accounts.stream()
-                .filter(account -> account.username.equals(username))
-                .findFirst()
-                .orElse(null);
-
-        if (existingAccount != null) {
-            System.out.println("Your account already exists");
+    public static void addToWallet(Object obj, String type) {
+        if ("money".equals(type)) {
+            balance += (int) obj;
+            System.out.println("The new balance after adding money is: " + balance);
         } else {
-            accounts.add(new Account(username, password));
-            System.out.println("Account created successfully!");
-        }
-    }
-
-    public static void login(String username, String password) {
-        Account foundAccount = accounts.stream()
-                .filter(account -> account.username.equals(username) && account.password.equals(password))
-                .findFirst()
-                .orElse(null);
-
-        if (foundAccount != null) {
-            isAuthenticated = true;
-            System.out.println("You are connected");
-        } else {
-            System.out.println("Please create your account");
-        }
-    }
-
-    public static void showBalance() {
-        if (isAuthenticated) {
-            System.out.println("The balance is: " + balance);
-        } else {
-            System.out.println("Please create your account");
-        }
-    }
-
-    public static void addMoney(double money) {
-        if (isAuthenticated) {
-            balance += money;
-        transactionList.add(money + "_addmoney");
-        System.out.println("The new balance after adding money is: " + balance);
-        } else {
-            System.out.println("Please create your account");
-        }
-        
-   }
-    public static void takeOutMoney(double money) {
-        if (isAuthenticated) {
-            if (money <= balance) {
-                balance -= money;
-                transactionList.add(money + "_takeoutmoney");
-                System.out.println("The new balance after taking out money is: " + balance);
+            List<String> selectedList = getListByType(type);
+            if (selectedList != null) {
+                selectedList.add(obj.toString());
+                System.out.println("You've added a " + type + " to your wallet: " + obj);
             } else {
-                System.out.println("The balance is insufficient");
+                System.out.println("It doesn't fit in your wallet.");
+            }
+        }
+    }
+
+    public static void removeFromWallet(Object obj, String type) {
+        if ("money".equals(type)) {
+            if (balance >= (int) obj) {
+                balance -= (int) obj;
+                System.out.println("You have withdrawn " + obj + " from your portfolio. Total amount: " + balance);
+            } else {
+                System.out.println("You don't have enough money in your wallet!");
             }
         } else {
-            System.out.println("Please create your account");
+            List<String> selectedList = getListByType(type);
+            if (selectedList != null) {
+                if (selectedList.contains(obj.toString())) {
+                    selectedList.remove(obj.toString());
+                    System.out.println("You have removed a " + type + " from your wallet");
+                } else {
+                    System.out.println("The " + type + " does not exist in your wallet!");
+                }
+            } else {
+                System.out.println("Object type not supported!");
+            }
         }
-        
     }
 
-    public static void showTransactionList(){
-        if (isAuthenticated) {
-           System.out.println("Transaction list: " + transactionList);
-        } else {
-            System.out.println("Please create your account");
+    public static void lookIntoWallet() {
+        System.out.println("The content of wallet");
+        System.out.println("Money: " + balance);
+        System.out.println("Images: " + images);
+        System.out.println("Credit cards: " + creditCards);
+        System.out.println("CIN: " + cin);
+        System.out.println("Driver's license: " + driverLicense);
+        System.out.println("Visiting cards: " + visitingCard);
+    }
+
+    private static List<String> getListByType(String type) {
+        switch (type) {
+            case "image":
+                return images;
+            case "credit-card":
+                return creditCards;
+            case "driver-license":
+                return driverLicense;
+            case "CIN":
+                return cin;
+            case "visiting-card":
+                return visitingCard;
+            default:
+                return null;
         }
-        
     }
 
     public static void main(String[] args) {
-        createAccount("username", "password");
-        login("username", "password");
-        addMoney(50);
-        takeOutMoney(40);
-        showTransactionList();
-        showBalance();
+        addToWallet(50, "money");
+        addToWallet("image1.jpg", "image");
+        addToWallet("BNI card", "credit-card");
+        addToWallet("CIN1", "CIN");
+        addToWallet("Driver license 1", "driver-license");
+        addToWallet("Doctor", "visiting-card");
+        lookIntoWallet();
+        removeFromWallet(20, "money");
+        lookIntoWallet();
     }
 }
